5d324d5e33044a4ccc3c80ebc0b98a2377b23ace
allen royston <allen.acr@gmail.com>
2016-08-08 16:37:15 -0700

    test

---
+++ README.md
@@ -0,0 +1,62 @@
+# ang2-parallax
+
+### What Am I?!
+An easy way to implement parallax scrolling for Angular2 components.  (There's also an [Angular 1.x](https://github.com/allenRoyston/ng-parallax "Angular 1.x") directive for those that need it.)
+  - No dependencies
+  - Responsive
+  - Simple
+  - Works for mobile!  (Well, iPhones at least - haven't tested on an Android yet)
+  - Tiny
+
+### Installation
+Include the module in your scripts.<br>
+```
+
+import { Component } from '@angular/core';
+import { ng2Parallax  } from '<pathTo>/ng2-parallax/parallax.directive';
+
+@Component({
+  selector: 'my-component',
+  directives: [ng2Parallax],
+  template:`
+  
+  <div style='width: 200; height: 200px'>
+    <div parallax speed="5" src="path/to/image.jpg"></div>
+  </div>
+  
+  `
+})
+export class componentName { }
+
+```
+
+### Version
+1.0.0
+
+
+### Live Demo 
+[Check it out](https://ng2-parallax-demo.herokuapp.com/ "ng2 Parallax Demo")
+
+
+### Dependencies
+- None!
+
+### NPM / Bower
+```
+npm install ang2-parallax --save-dev
+```
+
+
+
+
+### Parameters
+```
+speed = 0 to xx (the higher the number, the slower the effect).  This part requires some tinkering since different image sizes will respond differently.
+
+src = path to image
+```
+
+License
+----
+
+MIT - go nuts y'all.
---
+++ ng2-phaser/phaser.directive.js
@@ -0,0 +1,94 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+// core
+var core_1 = require('@angular/core');
+var common_1 = require('@angular/common');
+//------------------------------------
+var PhaserComponent = (function () {
+    //--------------
+    function PhaserComponent(el) {
+        this.el = el;
+        this.phaser = new core_1.EventEmitter();
+        this.selfRef = el.nativeElement;
+    }
+    //--------------
+    //--------------
+    PhaserComponent.prototype.ngOnInit = function () {
+        var t = this;
+        var alreadyLoaded = false;
+        var allScripts = document.getElementsByTagName("script");
+        for (var i = 0; i < allScripts.length; i++) {
+            var str = allScripts[i].src;
+            if (str.indexOf(t.settings.file) >= 0) {
+                alreadyLoaded = true;
+            }
+        }
+        if (alreadyLoaded) {
+            var scriptLoadedTest = function () {
+                setTimeout(function () {
+                    try {
+                        var test = new Phaser.Game();
+                        clearInterval(this);
+                    }
+                    catch (err) { }
+                    finally {
+                        if (test != undefined) {
+                            t.initPhaser();
+                        }
+                        else {
+                            scriptLoadedTest();
+                        }
+                    }
+                }, 1);
+            };
+            var intv = scriptLoadedTest();
+        }
+        else {
+            var js = document.createElement("script");
+            js.type = "text/javascript";
+            js.src = t.settings.file;
+            document.body.appendChild(js);
+            js.onload = function () {
+                t.initPhaser();
+            };
+        }
+    };
+    //--------------
+    //--------------
+    PhaserComponent.prototype.initPhaser = function () {
+        this.phaser.emit({ container: this.selfRef });
+    };
+    __decorate([
+        core_1.Output(), 
+        __metadata('design:type', Object)
+    ], PhaserComponent.prototype, "phaser", void 0);
+    __decorate([
+        core_1.Input(), 
+        __metadata('design:type', Object)
+    ], PhaserComponent.prototype, "settings", void 0);
+    __decorate([
+        core_1.Input(), 
+        __metadata('design:type', Object)
+    ], PhaserComponent.prototype, "layout", void 0);
+    PhaserComponent = __decorate([
+        core_1.Component({
+            selector: 'phaser',
+            directives: [common_1.CORE_DIRECTIVES],
+            template: ""
+        }), 
+        __metadata('design:paramtypes', [(typeof (_a = typeof core_1.ElementRef !== 'undefined' && core_1.ElementRef) === 'function' && _a) || Object])
+    ], PhaserComponent);
+    return PhaserComponent;
+    var _a;
+}());
+exports.PhaserComponent = PhaserComponent;
+//------------------------------------
+//# sourceMappingURL=phaser.directive.js.map
